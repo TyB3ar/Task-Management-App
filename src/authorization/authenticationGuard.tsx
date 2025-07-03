1df130f0ff5ae17191ca1893781f7bf7e9a48054
Tyler Wronski <wronskityler@gmail.com>
2025-07-03 13:00:25 -0700

    Added auth0 compnents, Login and Logout Buttons

---
+++ src/authorization/authenticationGuard.tsx
@@ -0,0 +1,18 @@
+import { withAuthenticationRequired } from "@auth0/auth0-react";
+import type React from "react";
+
+type AuthenticationGuardProps = {
+    component: React.ReactNode | any; 
+}
+
+const AuthenticationGuard: React.FC<AuthenticationGuardProps> = ({component}) => {
+    const Component = withAuthenticationRequired(component,{
+        onRedirecting: () => <div>Redirecting you to the login page...</div>,
+    });
+
+    return (
+        <Component /> 
+    );
+};
+
+export default AuthenticationGuard; 
